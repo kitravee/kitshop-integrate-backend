8367f8f88d7e81d17e875fea4cff005787cf7083
Gitravs <kitravee.swk@gmail.com>
2020-06-17 17:01:44 +0700

    update out application App singin and signup component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -17,77 +17,33 @@ import { createStructuredSelector } from "reselect";
 import "./App.css";
 
 //convert to class component because we whant to contain state
-class App extends React.Component {
-  // constructor() {
-  //   super();
-  //   //user that loging in
-  //   //comment out because of intregrad
-  //   this.state = {
-  //     currentUser: null,
-  //   };
-  // }
-
-  //create method equal to null
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
-  //   const { setCurrentUser } = this.props;
-  //   // when Auth state Change if refrash it will also return same user until you sign out
-  //   // ⭐ A Observable can have three different states as well : Subscribed, Error, Completed.
-  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-  //     if (userAuth) {
-  //       //createUserProfileDocument return documentReference
-  //       const UserRef = await createUserProfileDocument(userAuth);
+  }, [checkUserSession]);
 
-  //       UserRef.onSnapshot((snapShot) => {
-  //         setCurrentUser({
-  //           id: snapShot.id,
-  //           ...snapShot.data(),
-  //         });
-  //       });
-  //     } else {
-  //       // if there is not thing in userAuth
-  //       setCurrentUser(userAuth);
-  //     }
-  //   });
-  // }
-
-  componentWillUnmount() {
-    // #86 to unsubscribe/close auth
-
-    this.unsubscribeFromAuth();
-  }
-  // Route render for ()=>() inline function
-  // component for component
-  render() {
-    console.log(this.props.currentUser);
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
-
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            this.props.currentUser ? (
+              <Redirect to="/" />
+            ) : (
+              <SignInAndSignUpPage />
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 // to check CurrentUser for redirect to "/"
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
